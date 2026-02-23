1e15872122bf588d59497b8f37caa78c1abacacd
Taylor Fausak <tfausak@mercury.com>
2026-02-23 10:40:21 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "VS Code standalone CLI (code tunnel)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      version = "1.109.5";
+      commit = "072586267e68ece9a47aa43f8c108e0dcbf44622";
+
+      sources = {
+        aarch64-linux = {
+          url = "https://vscode.download.prss.microsoft.com/dbazure/download/stable/${commit}/vscode_cli_alpine_arm64_cli.tar.gz";
+          hash = "sha256-iZqKrO8sVH0BvLF5ILXFtyUbagJdT7Kw/Yh1BTrd09Y=";
+        };
+        x86_64-linux = {
+          url = "https://vscode.download.prss.microsoft.com/dbazure/download/stable/${commit}/vscode_cli_alpine_x64_cli.tar.gz";
+          hash = "sha256-dpnXzOC1Jktgi3o31saJ0E8L49uMCL9x/HPU7DWZ2iw=";
+        };
+        aarch64-darwin = {
+          url = "https://vscode.download.prss.microsoft.com/dbazure/download/stable/${commit}/vscode_cli_darwin_arm64_cli.zip";
+          hash = "sha256-PfrrPa8qbSrnniaiPaVo8QuQX+rBSfA/IajM8hOe3E0=";
+        };
+        x86_64-darwin = {
+          url = "https://vscode.download.prss.microsoft.com/dbazure/download/stable/${commit}/vscode_cli_darwin_x64_cli.zip";
+          hash = "sha256-0NTltR811cQ3y3+NAD0XutMzrvPWCPK2zQzu5kd42GA=";
+        };
+      };
+
+      forAllSystems = nixpkgs.lib.genAttrs (builtins.attrNames sources);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          src = pkgs.fetchurl {
+            inherit (sources.${system}) url hash;
+          };
+          isLinux = pkgs.lib.hasSuffix "linux" system;
+        in
+        {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "vscode-cli";
+            inherit version src;
+
+            sourceRoot = ".";
+
+            nativeBuildInputs = pkgs.lib.optionals (!isLinux) [ pkgs.unzip ];
+
+            dontConfigure = true;
+            dontBuild = true;
+
+            installPhase = ''
+              install -Dm755 code $out/bin/code
+            '';
+
+            meta = {
+              description = "Visual Studio Code CLI for remote tunnels";
+              homepage = "https://code.visualstudio.com";
+              license = pkgs.lib.licenses.unfree;
+              mainProgram = "code";
+              platforms = builtins.attrNames sources;
+            };
+          };
+        }
+      );
+    };
+}
